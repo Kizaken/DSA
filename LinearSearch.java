0a1e551d6035e061c2da14797b8489fe983cc8e5
Kizaken <lie.ruina@gmail.com>
2016-09-08 19:33:19 +0800

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,74 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class LinearSearch {
+	
+
+	int [] array;
+	double average = 0;
+	int arraySize;
+	private int toSearch;
+	
+	public LinearSearch (int size){
+		arraySize = size;
+		array = new int [arraySize];
+
+		
+	}
+	public void getArray(){
+		
+		Random rand = new Random();
+		
+		for (int i = 0; i < arraySize; i++){
+			array[i]=rand.nextInt(100);
+			System.out.println("Random[" +i+ "] = " + array[i]);
+		}
+		System.out.println();
+	}
+	
+	
+	public void show(){
+		
+		for (int i = 0; i < arraySize; i++){
+			
+			System.out.println ( array[i]);
+		}
+		
+	}
+	
+	public void search(){
+		Scanner input = new Scanner(System.in);
+		int i;
+		System.out.print("Plese enter the number to be search : ");
+		toSearch = input.nextInt();
+		
+		for(i = 0; i<array.length;i++){
+			if(toSearch == array[i]){
+				System.out.print("The value has been found in array["+i+"] = "+array[i]);
+				break;
+			}
+			
+		}
+		if(i == array.length){
+			System.out.print("The value not found!");		
+			}
+	}
+	
+	public static void main (String[] Args){
+		
+		int size;
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Enter array size: " );
+		size = input.nextInt();
+		
+		
+		LinearSearch summon = new LinearSearch (size);
+		summon.getArray();
+		summon.search();
+		
+		
+		
+	
+	}
+}
